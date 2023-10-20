73078c5d4abf146b7d4bc1e53aa9fb26582fd231
VISHAL SARAIWAL <71539186+kritriv@users.noreply.github.com>
2023-10-20 10:44:06 +0530

    Create Multi-lang_Api.js

---
+++ Multi-lang_Api.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {
+      new google.translate.TranslateElement(
+        { pageLanguage: 'en' },
+        'google_translate_element'
+      );
+      document.getElementById('google_translate_element').style.background="#00A0EB";
+    }
